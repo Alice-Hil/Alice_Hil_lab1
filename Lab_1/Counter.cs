5d784edf8a68654142d460ea0d6a67b5d933362c
Alice-Hil <88400580+Alice-Hil@users.noreply.github.com>
2022-05-12 10:44:30 +0200

    Add files via upload

---
+++ Lab_1/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_1
+{
+	public class Counter
+	{
+		private int startValue;
+		private int finishValue;
+		private int currentNum;
+		public int StartValue { get { return startValue; } set { startValue = value; } }
+		public int FinishValue { get { return finishValue; } set { finishValue = value; } }
+		public int CurrentNum { get { return currentNum; } set { currentNum = value; } }
+		
+
+		public Counter(int startPoint = 1, int finishPoint = 10)
+		{
+			startValue = startPoint;
+			finishValue = finishPoint;
+			currentNum = startValue;
+		}
+
+		public void PrintCurrent()
+        {
+			Console.WriteLine("Current num is " + currentNum);
+        }
+
+		public void PlusOne()
+        {
+			currentNum++;
+			if(currentNum > finishValue)
+            {
+				currentNum--;
+				Console.WriteLine("Impossible to change - reached max value");
+            }
+        }
+
+		public void MinusOne()
+		{
+			currentNum--;
+			if (currentNum < startValue)
+			{
+				currentNum++;
+				Console.WriteLine("Impossible to change - reached min value");
+			}
+		}
+	}
+}
---
+++ Lab_1/FileJSON.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class FileJSON
+    {
+        public static void SafeTo(Counter counter)
+        {
+            string objectToSerialize = JsonConvert.SerializeObject(counter);
+            File.WriteAllText("C:/Users/Алиса/source/repos/Lab_1/counter.json", objectToSerialize);
+        }
+
+        public static Counter ReadFrom(string path)
+        {
+            string json = File.ReadAllText("C:/Users/Алиса/source/repos/Lab_1/counter.json");
+            Counter counter = JsonConvert.DeserializeObject<Counter>(json);
+            return counter;
+        }
+    }
+}
